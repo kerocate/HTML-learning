cd9de9d768699550f76c8a81f0ca7171b5cc4bbe
Kerocate_军喵 <kerocate_moe@outlook.com>
2019-05-12 00:40:16 +0800

    添加了注释以免遗忘

--- MoveAnimation/main.js
+++ MoveAnimation/main.js
@@ -2,17 +2,20 @@ function $(elm){
    return document.querySelector(elm);
 }
 
-function move(location){
-    for(i=0;i<location;i++){
+function move(location) {
+    for (i = 0; i < location; i++) {
+        //闭包解决计数困境
         (function (i) {
+            //正常递加的i作为等待时间函数的参数
+            //由于第一个参数是function 所以用了箭头函数表达式
             setTimeout(() => {
-                $("#box").style.left=i+"px";
-                if (i==99) {
+                $("#box").style.left = i + "px";
+                if (i == 99) {
                     // 这样子在结尾不会抛出结果
                     console.log(i);
                     move1(100);
                 }
-            }, i*5);
+            }, i * 5);
         })(i);
         // for(j=0;j<location;j++){
         //     (function (j) {
